abb039922e1707f04f0a3eb900360b7ca1a201b4
VT-Zhang <jianz@vt.edu>
2017-05-10 00:19:06 -0400

    algos

--- Leetcodes/448_FindDisappearedNumbers.js
+++ Leetcodes/448_FindDisappearedNumbers.js
@@ -1,4 +1,5 @@
 var findDisappearedNumbers = function(nums) {
+    //sort the array first
     for(var i=0; i<nums.length; i++){
         var min = i;
         for(var j=i; j<nums.length; j++){
@@ -12,12 +13,21 @@ var findDisappearedNumbers = function(nums) {
             nums[min] = temp;
         }
     }
+    //remove the duplicated elements
     for(var k=0; k<nums.length; k++){
-        if(nums[k]=nums[k+1]){
+        if(nums[k] == nums[k+1]){
             nums.splice(k,1);
         }
     }
-    console.log(nums);
+    //find the missing numbers
+    var result = [];
+    for(var l=0;l<nums.length; l++){
+        if(nums[l]+1 != nums[l+1]){
+            // nums.splice(l+1,0,nums[l]+1);
+            result.push(nums[l]+1);
+        }
+    }
+    console.log(result);
 };
 
 findDisappearedNumbers([4,3,2,7,8,2,3,1]);
