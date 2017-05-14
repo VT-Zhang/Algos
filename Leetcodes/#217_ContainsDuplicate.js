75881fc90c36301b465bbdee44decf27b3d4086a
VT-Zhang <jianz@vt.edu>
2017-05-13 20:11:35 -0400

    Contains Duplicate.js

--- Leetcodes/#217_ContainsDuplicate.js
+++ Leetcodes/#217_ContainsDuplicate.js
@@ -3,16 +3,35 @@
 // return false if every element is distinct.
 
 var containsDuplicate = function(nums) {
+    if(nums===null){
+        return false;
+    }
+    var dict = {};
     for(var i=0; i<nums.length; i++){
-        for(var j=i+1; j<nums.length; j++){
-            if(nums[i]===nums[j]){
-                console.log(true);
-                return true;
-            }
+        var val = nums[i];
+        if(!dict[val]){
+            dict[val] = "yes";
+            console.log(dict);
+        }
+        else{
+            return true;
         }
     }
-    console.log(false);
-    return false
+    return false;
+
+    // double for loop method will return runtime overtime errors.
+
+    // for(var i=0; i<nums.length; i++){
+    //     for(var j=i+1; j<nums.length; j++){
+    //         if(nums[i]===nums[j]){
+    //             console.log(true);
+    //             return true;
+    //         }
+    //     }
+    // }
+    // console.log(false);
+    // return false
 };
 
-containsDuplicate([5,6,5,4,2,3,1]);
+console.log(containsDuplicate([5,6,5,4,2,3,1]));
+console.log(containsDuplicate([5,6,4,2,3,1]));
