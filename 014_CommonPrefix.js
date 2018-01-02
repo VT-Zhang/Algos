274e4cd7b7f069a65251aad4938e8a7b323f2ff3
VT-Zhang <jianz@vt.edu>
2018-01-02 11:53:28 -0500

    Common Prefix

---
+++ 014_CommonPrefix.js
@@ -0,0 +1,30 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+var longestCommonPrefix = function(strs) {
+    if(strs.length == 0){
+        return "";
+    }
+    if(strs.length == 1) {
+        return strs[0];
+    }
+    var prefix = "";
+    var temp = "";
+    for(var i = 0; i < strs[0].length; i++) {
+        temp += strs[0][i];
+    }
+    for(var j = 0; j < temp.length; j++) {
+        for(var k = 1; k < strs.length; k++) {
+            if(strs[k][j] != temp[j]) {
+                return prefix;
+            }
+        }
+        prefix += temp[j];
+    }
+    return prefix;
+};
+
+console.log(longestCommonPrefix(['predetermine', 'predator', 'previous']));
+console.log(longestCommonPrefix(['condo', 'congrats', 'convict']));
+console.log(longestCommonPrefix(['com', 'com', 'com']));
+console.log(longestCommonPrefix([]));
+console.log(longestCommonPrefix(["a"]));
