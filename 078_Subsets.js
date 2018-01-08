f62ae372f5163fd1cc6d2994fc86251989fc1bf6
Jian Zhang <jianz@vt.edu>
2018-01-07 21:51:17 -0500

    Subests

---
+++ 078_Subsets.js
@@ -0,0 +1,42 @@
+// Given a set of distinct integers, nums, return all possible subsets.
+
+// Note: The solution set must not contain duplicate subsets.
+
+// For example,
+// If nums = [1,2,3], a solution is:
+
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1,2,3],
+//   [1,3],
+//   [2,3],
+//   [1,2],
+//   []
+// ]
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    nums.sort(function (a, b) {
+        return a - b;
+    });
+    var res = [[]];
+    var length = Math.pow(2, nums.length);
+    for(var i = 0; i < nums.length; i++) {
+        for(var j = 0; j < length; j++) {
+            if((j >> i) & 1) {
+                if(!res[j]) {
+                    res[j] = [];
+                }
+                res[j].push(nums[i]);
+            }
+        }
+    }
+    return res;
+};
+
+console.log(subsets([2,1,3]));
