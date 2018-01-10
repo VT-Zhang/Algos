89e0d8b9f9dadfb6f7466fedfc23ba520764d45a
VT-Zhang <jianz@vt.edu>
2018-01-10 13:27:33 -0500

    Merge Two Sorted List

---
+++ 556_NextGreaterElementIII.js
@@ -0,0 +1,25 @@
+// Given a positive 32-bit integer n, you need to find the smallest 32-bit integer which has exactly the same digits
+// existing in the integer n and is greater in value than n. If no such positive 32-bit integer exists, you need to return -1.
+//
+// Example 1:
+// Input: 12
+// Output: 21
+// Example 2:
+// Input: 21
+// Output: -1
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nextGreaterElement = function(n) {
+    var arr = [];
+    var str = n.toString();
+    for(var i = 0; i < str.length; i++) {
+        var temp = "";
+
+    }
+
+}
+
+console.log(nextGreaterElement(231));
