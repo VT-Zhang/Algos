e85d2f91c6c41b0ad7f77abd2d57c534132bb6db
VT-Zhang <jianz@vt.edu>
2018-05-11 10:22:38 -0400

    513 Find Bottomleft Tree Value

---
+++ 513_FindBottomLeftTreeValue.js
@@ -0,0 +1,59 @@
+// Given a binary tree, find the leftmost value in the last row of the tree.
+//
+//     Example 1:
+// Input:
+//
+//      2
+//     / \
+//    1   3
+//
+// Output:
+//     1
+// Example 2:
+// Input:
+//
+//      1
+//     / \
+//    2   3
+//   /   / \
+//  4   5   6
+//     /
+//    7
+//
+// Output:
+//     7
+// Note: You may assume the tree (i.e., the given root node) is not NULL.
+
+/**
+ * Definition for a binary tree node.
+ */
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function findBottomLeftValue(root) {
+    var res = {
+        max: 1,
+        leftBottom: root.val
+    };
+    return helper(root, 1, res);
+}
+
+function helper(root, level, res) {
+    if (res.max < level) {
+        res.max = level;
+        res.leftBottom = root.val;
+    }
+    if (root.left) {
+        helper(root.left, level + 1, res);
+    }
+    if (root.right) {
+        helper(root.right, level + 1, res);
+    }
+    return res.leftBottom;
+}
