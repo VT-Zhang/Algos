60c5486155d83c5faff3aefda49fff8444b82abb
VT-Zhang <jianz@vt.edu>
2018-06-04 17:09:45 -0400

    Utopian Tree

---
+++ HackerRank/UtopianTree.js
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/utopian-tree/problem
+
+function utopianTree(n) {
+    let height = 1;
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            height *= 2;
+        } else {
+            height++;
+        }
+        console.log(height);
+    }
+}
+
+console.log(utopianTree(4));
