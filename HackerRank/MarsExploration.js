8dcd451a2175a7d8730c27c9ea7a22251eef1d8a
VT-Zhang <jianz@vt.edu>
2018-06-05 11:55:56 -0400

    Mars Exploration

---
+++ HackerRank/MarsExploration.js
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/mars-exploration/problem
+
+function marsExploration(s) {
+    let count = 0;
+    for (let i = 0; i < s.length; i += 3) {
+        if (s[i] !== "S") {
+            count++;
+        }
+        if (s[i+1] !== "O") {
+            count++;
+        }
+        if (s[i+2] !== "S") {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(marsExploration("SOSSPSSQSSOR"));
+console.log(marsExploration("SOSSOSSOS"));
+console.log(marsExploration("SOSOOSOSOSOSOSSOSOSOSOSOSOS"));
