ec34e8d1673595e57fc05df5324a2fdb0a268476
VT-Zhang <jianz@vt.edu>
2018-06-12 14:02:40 -0400

    Added validate function

--- HackerRank/WeatherConstructor.js
+++ HackerRank/WeatherConstructor.js
@@ -8,4 +8,21 @@ function weatherConstructor(timestamp, temperature, dewPoint, precipitation) {
     return obj;
 }
 
-console.log(weatherConstructor("01-01-2018", 55, 35, 10));
+// console.log(weatherConstructor("01-01-2018", 55, 35, 10));
+
+function validate(obj) {
+    if (!obj.timestamp) {
+        return false;
+    }
+    let re = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+    if (re.test(obj.timestamp) &&
+        obj.temperature === parseFloat(obj.temperature) &&
+        obj.dewPoint === parseFloat(obj.dewPoint) &&
+        obj.precipitation === parseFloat(obj.precipitation)) {
+        return true;
+    }
+    return false;
+}
+
+let obj = weatherConstructor("2012-10-06T04:13:00+00:00", 55, 35, 10);
+console.log(validate(obj));
