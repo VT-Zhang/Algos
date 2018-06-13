fcf0504f4ea4cacecf3b1acbb6e77595119af534
VT-Zhang <jianz@vt.edu>
2018-06-13 13:57:02 -0400

    Added documentation and refactored a few lines

--- NodeServer/measurements.js
+++ NodeServer/measurements.js
@@ -96,6 +96,11 @@ let database = [
 
 // let database = [];
 
+/**
+ * Function to validate the input object.
+ * @param obj, the input object from the request.body.
+ * @returns {boolean}, return true if the input object is valid, false otherwise.
+ */
 function validateInput(obj) {
     const re = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
     if (!obj.timestamp || !re.test(obj.timestamp)) {
@@ -113,26 +118,40 @@ function validateInput(obj) {
     return true;
 }
 
-
+/**
+ * Function to get the max value from an array.
+ * @param arr, the array.
+ * @returns {string}, return the max value with 1 decimal place.
+ */
 function getMax(arr) {
     let max = Number.MIN_SAFE_INTEGER;
-    arr.forEach(function(item) {
+    arr.forEach(function (item) {
         max = Math.max(max, item);
     });
     return max.toFixed(1);
 }
 
+/**
+ * Function to get the min value from an array.
+ * @param arr, the array.
+ * @returns {string}, return the min value with 1 decimal place.
+ */
 function getMin(arr) {
     let min = Number.MAX_SAFE_INTEGER;
-    arr.forEach(function(item) {
+    arr.forEach(function (item) {
         min = Math.min(min, item);
     });
     return min.toFixed(1);
 }
 
+/**
+ * Function to get the average value from an array.
+ * @param arr, the array.
+ * @returns {string}, return the average value with 1 decimal place.
+ */
 function getAvg(arr) {
     let sum = 0;
-    arr.forEach(function(item) {
+    arr.forEach(function (item) {
         sum += item;
     });
     return (sum / arr.length).toFixed(1);
@@ -140,15 +159,28 @@ function getAvg(arr) {
 
 module.exports = {
 
-    showAll: function(req, res) {
+    /**
+     * Function to return all the measurements from the database.
+     * @param req
+     * @param res
+     * @returns {*|void}
+     */
+    showAll: function (req, res) {
         return res.status(200).send(database);
     },
 
-    show: function(req, res) {
+    /**
+     * Function to return a specific measurement from a datetime
+     * As well as to return an array of measurements from a certain date.
+     * @param req
+     * @param res
+     * @returns {*}
+     */
+    show: function (req, res) {
         const date = req.params.date;
         if (date.length === 10) {
             let result = [];
-            database.forEach(function(item) {
+            database.forEach(function (item) {
                 if (item.timestamp.includes(date)) {
                     result.push(item);
                 }
@@ -158,7 +190,7 @@ module.exports = {
             }
             return res.sendStatus(404);
         } else {
-            database.forEach(function(item) {
+            database.forEach(function (item) {
                 if (item.timestamp === date) {
                     return res.status(200).send(item);
                 }
@@ -167,7 +199,13 @@ module.exports = {
         }
     },
 
-    create: function(req, res) {
+    /**
+     * Function to create a new measurement and save it in the database.
+     * @param req
+     * @param res
+     * @returns {*}
+     */
+    create: function (req, res) {
         if (!req.body) {
             return res.sendStatus(400);
         } else {
@@ -180,14 +218,20 @@ module.exports = {
         }
     },
 
-    update: function(req, res) {
+    /**
+     * Function to update a measurement (to replace all the values in the object).
+     * @param req
+     * @param res
+     * @returns {*}
+     */
+    update: function (req, res) {
         const date = req.params.date;
         const measurement = req.body;
         if (date !== measurement.timestamp) {
             return res.sendStatus(409);
         }
         if (validateInput(measurement)) {
-            database.forEach(function(item) {
+            database.forEach(function (item) {
                 if (item.timestamp === date) {
                     item.temperature = measurement.temperature;
                     item.dewPoint = measurement.dewPoint;
@@ -200,14 +244,20 @@ module.exports = {
         return res.sendStatus(400);
     },
 
-    patch: function(req, res) {
+    /**
+     * Function to partially update a measurement (to replace a certain values in the object).
+     * @param req
+     * @param res
+     * @returns {*}
+     */
+    patch: function (req, res) {
         const date = req.params.date;
         const timestamp = req.body.timestamp;
         if (date !== timestamp) {
             return res.sendStatus(409);
         }
         if (validateInput(req.body)) {
-            database.forEach(function(item) {
+            database.forEach(function (item) {
                 if (item.timestamp === date) {
                     if (req.body.temperature) {
                         item.temperature = req.body.temperature;
@@ -226,9 +276,15 @@ module.exports = {
         return res.sendStatus(400);
     },
 
-    delete: function(req, res) {
+    /**
+     * Function to delete a measurement from the database.
+     * @param req
+     * @param res
+     * @returns {*}
+     */
+    delete: function (req, res) {
         const date = req.params.date;
-        for(let i = 0; i < database.length; i++) {
+        for (let i = 0; i < database.length; i++) {
             if (date === database[i].timestamp) {
                 database.splice(i, 1);
                 return res.sendStatus(204);
@@ -239,13 +295,20 @@ module.exports = {
 // http://localhost:5000/stats?stat=min&stat=max&stat=average&metric=temperature&fromDateTime=2015-09-01T16:00:00.000Z&toDateTime=2015-09-01T17:00:00.000Z
 // http://localhost:5000/stats?stat=min&stat=max&stat=average&metric=dewPoint&fromDateTime=2015-09-01T16:00:00.000Z&toDateTime=2015-09-01T17:00:00.000Z
 
-    stats: function(req, res) {
+    /**
+     * Function to query the database and return a certain array of measurements
+     * According to the query critera.
+     * @param req
+     * @param res
+     * @returns {*|void}
+     */
+    stats: function (req, res) {
         let result = [];
         let dateRange = [];
-        console.log(req.query);
+        // console.log(req.query);
 
-        var stats = [];
-        var metrics = [];
+        let stats = [];
+        let metrics = [];
 
         if (typeof(req.query.stat) === "string") {
             stats.push(req.query.stat);
@@ -261,41 +324,37 @@ module.exports = {
         const left = new Date(req.query.fromDateTime);
         const right = new Date(req.query.toDateTime);
 
-        database.forEach(function(item) {
+        database.forEach(function (item) {
             let date = new Date(item.timestamp);
             if (date >= left && date < right) {
                 dateRange.push(item);
             }
         });
-        // console.log(dateRange);
-        
-        metrics.forEach(function(metric) {
-            stats.forEach(function(stat) {
-                let temp = [];
-                dateRange.forEach(function(date) {
+
+        metrics.forEach(function (metric) {
+            stats.forEach(function (stat) {
+                let valueArray = [];
+                dateRange.forEach(function (date) {
                     if (metric === "temperature" && date.temperature) {
-                        temp.push(date.temperature);
+                        valueArray.push(date.temperature);
                     }
                     if (metric === "dewPoint" && date.dewPoint) {
-                        temp.push(date.dewPoint);
+                        valueArray.push(date.dewPoint);
                     }
                     if (metric === "precipitation" && date.precipitation) {
-                        temp.push(date.precipitation);
+                        valueArray.push(date.precipitation);
                     }
                 });
-                console.log(temp);
-                if (temp.length !== 0) {
+                // console.log(valueArray);
+                if (valueArray.length !== 0) {
                     if (stat === "min") {
-                        let min = getMin(temp);
-                        result.push({"metric": metric, "stat": stat, "value": min});
+                        result.push({"metric": metric, "stat": stat, "value": getMin(valueArray)});
                     }
                     if (stat === "max") {
-                        let max = getMax(temp);
-                        result.push({"metric": metric, "stat": stat, "value": max});
+                        result.push({"metric": metric, "stat": stat, "value": getMax(valueArray)});
                     }
                     if (stat === "average") {
-                        let avg = getAvg(temp);
-                        result.push({"metric": metric, "stat": stat, "value": avg});
+                        result.push({"metric": metric, "stat": stat, "value": getAvg(valueArray)});
                     }
                 }
             });
