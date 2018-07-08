ed28be69cea6635ae3d9ecaa5e98f1e3e6c424bb
VT-Zhang <jianz@vt.edu>
2018-07-08 10:25:50 -0400

    Valid Parentheses

--- 020_ValidParentheses.js
+++ 020_ValidParentheses.js
@@ -4,36 +4,56 @@
 // The brackets must close in the correct order, "()" and "()[]{}" are all valid
 // but "(]" and "([)]" are not.
 
-function isValid (s) {
-    // 1. in pairs
-    // 2. in orders
-    // 3. open first, close last
+// function isValid (s) {
+//     // 1. in pairs
+//     // 2. in orders
+//     // 3. open first, close last
+//
+//     var map = {
+//         '(': ')',
+//         '[': ']',
+//         '{': '}'
+//     }
+//     console.log(map);
+//     var stack = [];
+//     for (var i = 0; i < s.length; i++) {
+//         if (s[i] in map) {
+//             stack.push(s[i]);
+//         }
+//         for (var key in map) {
+//             if (map[key] === s[i]) {
+//                 if (stack.length > 0 && map[stack[stack.length - 1]] === s[i]) {
+//                     stack.pop();
+//                 } else {
+//                     return false;
+//                 }
+//             }
+//         }
+//         console.log(stack);
+//     }
+//     return stack.length === 0;
+// }
 
-    var map = {
-        '(': ')',
-        '[': ']',
-        '{': '}'
-    }
-    console.log(map);
-    var stack = [];
-    for (var i = 0; i < s.length; i++) {
-        if (s[i] in map) {
-            stack.push(s[i]);
+function isValid (s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(')');
         }
-        for (var key in map) {
-            if (map[key] === s[i]) {
-                if (stack.length > 0 && map[stack[stack.length - 1]] === s[i]) {
-                    stack.pop();
-                } else {
-                    return false;
-                }
-            }
+        else if (s[i] === '[') {
+            stack.push(']');
+        }
+        else if (s[i] === '{') {
+            stack.push('}');
+        }
+        else if (stack.length === 0 || stack.pop() !== s[i]) {
+            return false;
         }
         console.log(stack);
     }
     return stack.length === 0;
 }
 
-// console.log(isValid("()[]{}"));
 console.log(isValid('([)]}'));
 console.log(isValid('()[]{}'));
+console.log(isValid('([]{})'));
