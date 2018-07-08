f48ff46d585304fa85d88e05980c5330e62d6024
VT-Zhang <jianz@vt.edu>
2018-07-08 13:46:33 -0400

    Subset

--- 078_Subsets.js
+++ 078_Subsets.js
@@ -16,27 +16,45 @@
 //   []
 // ]
 
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var subsets = function(nums) {
-    nums.sort(function (a, b) {
-        return a - b;
-    });
-    var res = [[]];
-    var length = Math.pow(2, nums.length);
-    for(var i = 0; i < nums.length; i++) {
-        for(var j = 0; j < length; j++) {
-            if((j >> i) & 1) {
-                if(!res[j]) {
-                    res[j] = [];
-                }
-                res[j].push(nums[i]);
-            }
-        }
+// /**
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+// var subsets = function(nums) {
+//     nums.sort(function (a, b) {
+//         return a - b;
+//     });
+//     var res = [[]];
+//     var length = Math.pow(2, nums.length);
+//     for(var i = 0; i < nums.length; i++) {
+//         for(var j = 0; j < length; j++) {
+//             if((j >> i) & 1) {
+//                 if(!res[j]) {
+//                     res[j] = [];
+//                 }
+//                 res[j].push(nums[i]);
+//             }
+//         }
+//     }
+//     return res;
+// };
+
+function subsets(arr) {
+    if (arr.length === 0 || arr === null) {
+        return [];
     }
+    let res = [[]];
+    arr.forEach( function (n) {
+        let length = res.length;
+        let i = 0;
+        while (i < length) {
+            let subset = res[i].slice(0);
+            subset.push(n);
+            res.push(subset);
+            i++;
+        }
+    });
     return res;
-};
+}
 
 console.log(subsets([2,1,3]));
