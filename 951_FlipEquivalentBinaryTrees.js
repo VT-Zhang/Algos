22dfcc48f71ae32f18bb0ecf0b1d98fee319de95
VT-Zhang <jianz@vt.edu>
2019-01-23 23:04:31 -0500

    951 Flip Equivalent Binary Trees

---
+++ 951_FlipEquivalentBinaryTrees.js
@@ -0,0 +1,23 @@
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+function flipEquiv(root1, root2) {
+    if (!root1 || !root2) {
+        return root1 === root2;
+    }
+    if (root1.val !== root2.val) {
+        return false;
+    }
+    return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) ||
+        (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+}
+
+/**
+ * Definition for a binary tree node.
+ */
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
