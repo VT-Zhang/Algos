46bd2089b9ba59a0f3f06f0283acc7cdfbaf7a09
VT-Zhang <jianz@vt.edu>
2019-02-05 23:29:25 -0500

    646 Maximum Length of Paired Chain

--- 646_MaximumLengthOfPairedChain.js
+++ 646_MaximumLengthOfPairedChain.js
@@ -15,20 +15,23 @@
  * @param {number[][]} pairs
  * @return {number}
  */
-var findLongestChain = function(pairs) {
-    pairs.sort(function (a, b) {
-        return a[1] - b[1];
-    });
-    console.log(pairs);
-    var stack = [pairs[0]];
-    for(var i = 1; i < pairs.length; i++) {
-        if(pairs[i][0] >= stack[stack.length-1][1]) {
-            stack.push(pairs[i]);
+function findLongestChain(pairs) {
+    if (pairs.length <= 0) {
+        return 0;
+    }
+    pairs.sort((a, b) =>
+        a[1] - b[1]
+    );
+    let res = 1;
+    let right = pairs[0][1];
+    for (let i = 1; i < pairs.length; i++) {
+        if (right < pairs[i][0]) {
+            res++;
+            right = pairs[i][1];
         }
     }
-    console.log(stack);
-    return stack.length - 1;
-};
+    return res;
+}
 
 // console.log(findLongestChain([[1,2], [2,3], [3,4]]));
 // console.log(findLongestChain([[1,2], [2,3], [3,4], [1,2],[2,3],[3,4],[4,5]]));
