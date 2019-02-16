37bbd1e5c545ff94bf364646520673e9c05b2520
VT-Zhang <jianz@vt.edu>
2019-02-16 14:35:55 -0500

    599 Minimum Index Sum of Two Lists

--- 599_MinimumIndexSumofTwoLists.js
+++ 599_MinimumIndexSumofTwoLists.js
@@ -1,8 +1,8 @@
-// Suppose Andy and Doris want to choose a restaurant for dinner, 
+// Suppose Andy and Doris want to choose a restaurant for dinner,
 // and they both have a list of favorite restaurants represented by strings.
 
-// You need to help them find out their common interest with the least 
-// list index sum. If there is a choice tie between answers, output all 
+// You need to help them find out their common interest with the least
+// list index sum. If there is a choice tie between answers, output all
 // of them with no order requirement. You could assume there always exists
 //  an answer.
 
@@ -17,7 +17,7 @@
 // ["Shogun", "Tapioca Express", "Burger King", "KFC"]
 // ["KFC", "Shogun", "Burger King"]
 // Output: ["Shogun"]
-// Explanation: The restaurant they both like and have the least index 
+// Explanation: The restaurant they both like and have the least index
 // sum is "Shogun" with index sum 1 (0+1).
 // Note:
 // The length of both lists will be in the range of [1, 1000].
@@ -25,31 +25,30 @@
 // The index is starting from 0 to the list length minus 1.
 // No duplicates in both lists.
 
-var findRestaurant = function(arr1, arr2) {
-    var restaurant = [];
-    var rank = [];
-    for(let i = 0; i < arr1.length; i++){
-        for(let j = 0; j < arr2.length; j++){
-            if(arr1[i] === arr2[j]){
-                restaurant.push(arr1[i]);
-                rank.push(i+j);
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+function findRestaurant(list1, list2) {
+    let common = [];
+    list1.forEach((res, index1) => {
+        let index2 = list2.indexOf(res);
+        if (index2 !== -1) {
+            let indexSum = index1 + index2;
+            if (!common[indexSum]) {
+                common[indexSum] = [];
             }
+            common[indexSum].push(res);
+        }
+    });
+
+    for (let i = 0; i < common.length; i++) {
+        if (common[i]) {
+            return common[i];
         }
     }
-    console.log(restaurant);
-    console.log(rank);
-    for(let i = 0; i < rank.length; i++){
-        var position = 0;
-        var min = rank[i];
-        if(rank[i] < min){
-            min = rank[i];
-            position = i;
-        }
-    }
-    console.log(position);
-    console.log(restaurant[position]);
-    return [restaurant[position]];
-};
+}
 
 findRestaurant(["Shogun", "Tapioca Express", "Burger King", "KFC"], ["KFC", "Shogun", "Burger King"])
 findRestaurant(["Shogun", "Tapioca Express", "Burger King", "KFC"],
