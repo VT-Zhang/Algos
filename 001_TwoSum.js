f13f7770a676c98f4511c6ba8d9b56992ef526c5
VT-Zhang <jianz@vt.edu>
2019-02-19 21:50:50 -0500

    001 Two Sum

--- 001_TwoSum.js
+++ 001_TwoSum.js
@@ -8,15 +8,17 @@
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
 
-var twoSum = function(nums, target) {
-    for (var i = 0; i < nums.length; i++){
-        for (var j = i+1; j< nums.length; j++){
-            if (nums[i] + nums[j] == target){
-                console.log([i, j]);
-                return [i, j];
-            }
+let twoSum = function(nums, target) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let another = target - nums[i];
+
+        if (another in map) {
+            return [map[another], i];
         }
+        map[nums[i]] = i;
     }
+    return null;
 };
 
 twoSum([2,7,11,15], 9);
