2e2745f1b739e8c286b2bde68f983e9491153ca6
VT-Zhang <jianz@vt.edu>
2019-04-01 22:09:22 -0400

    678 Valid Parenthesis String

---
+++ 678_ValidParenthesisString.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function checkValidString(s) {
+    let low = 0;
+    let high = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            low++;
+            high++;
+        } else if (s[i] === ')') {
+            if (low > 0) {
+                low--;
+            }
+            high--;
+        } else {
+            if (low > 0) {
+                low--;
+            }
+            high++;
+        }
+        if (high < 0) {
+            return false;
+        }
+    }
+    return low === 0;
+}
+
+console.log(checkValidString("(*)"));
+console.log(checkValidString("(*))"));
+console.log(checkValidString("(*))("));
