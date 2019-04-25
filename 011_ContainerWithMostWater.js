f27dc9ab9fe30881e6ed6b7b9ef7c890200987b9
VT-Zhang <jianz@vt.edu>
2019-04-24 22:04:39 -0400

    011 Container with Most Water

---
+++ 011_ContainerWithMostWater.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+function maxArea(height) {
+    let max = 0;
+    let left = 0;
+    let right = height.length - 1;
+    while (left < right) {
+        max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return max;
+}
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
