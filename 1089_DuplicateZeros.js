0af08cff008bf7e28890939ec7f90beb1a6b71b1
VT-Zhang <jianz@vt.edu>
2019-07-29 21:47:09 -0400

    1089 Duplicate Zeros

---
+++ 1089_DuplicateZeros.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+function duplicateZeros(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            for (let j = arr.length - 1; j > i; j--) {
+                arr[j] = arr[j-1];
+            }
+            i++;
+        }
+    }
+}
+
+console.log(duplicateZeros([1,0,2,3,0,4,5,0]));
+console.log(duplicateZeros([1,2,3]));
