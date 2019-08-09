1754d444ba2af2336820f5749820d93e70987efe
VT-Zhang <jianz@vt.edu>
2019-08-09 13:19:41 -0400

    1079 Ltter Tile Possibilities

---
+++ 1079_LetterTilePossibilities.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} tiles
+ * @return {number}
+ */
+function numTilePossibilities(tiles) {
+    let map = new Map();
+    for (let i = 0; i < tiles.length; i++) {
+        map.set(tiles[i], (map.get(tiles[i]) || 0) + 1);
+    }
+    return helper(map);
+
+    function helper(map) {
+        let sum = 0;
+        for (let [key, value] of map.entries()) {
+            if (value === 0) {
+                continue;
+            }
+            sum++;
+            map.set(key, map.get(key) - 1);
+            sum += helper(map, sum);
+            map.set(key, map.get(key) + 1);
+        }
+        return sum;
+    }
+}
+
+console.log(numTilePossibilities("AAB"));
+console.log(numTilePossibilities("AAABBC"));
