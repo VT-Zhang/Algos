02dbe64fe94de4fffd91227aa37ce061ffc9af56
VT-Zhang <jianz@vt.edu>
2019-09-19 23:06:57 -0400

    1103 Distribute Candies to People

---
+++ 1103_DistributeCandiesToPeople.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} candies
+ * @param {number} num_people
+ * @return {number[]}
+ */
+function distributeCandies(candies, num_people) {
+    let res = new Array(num_people).fill(0);
+    for (let i = 0; candies > 0; i++) {
+        res[i % num_people] += Math.min(candies, i + 1);
+        candies -= i + 1;
+    }
+    return res;
+}
+
+console.log(distributeCandies(7, 4));
+console.log(distributeCandies(10, 3));
