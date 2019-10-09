b1982db2f324b065ae50eca7cb8b2eb271357952
VT-Zhang <jianz@vt.edu>
2019-10-09 16:57:08 -0400

    372 Super Pow

---
+++ 372_SuperPow.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} a
+ * @param {number[]} b
+ * @return {number}
+ */
+function superPow(a, b) {
+    let res = 1;
+    for (let n of b) {
+        let next = 1;
+        for (let i = 0; i < 10; i++) {
+            next *= res;
+            next %= 1337;
+        }
+        for (let i = 0; i < n; i++) {
+            next *= a;
+            next %= 1337;
+        }
+        res = next;
+    }
+    return res;
+}
+
+console.log(superPow(2, [3]));
+console.log(superPow(2, [1, 0]));
