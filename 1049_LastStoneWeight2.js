2af57e3effeb4c442c9b8a58d713f60dd1f4b38c
VT-Zhang <jianz@vt.edu>
2019-10-15 23:01:24 -0400

    1049 Last Stone Weight II

---
+++ 1049_LastStoneWeight2.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+function lastStoneWeightII(stones) {
+    let dp = [], sum = 0;
+    dp[0] = true;
+    stones.forEach(stone => {
+        sum += stone;
+        for (let i = sum; i >= stone; i--) {
+            if (dp[i - stone]) {
+                dp[i] = true;
+            }
+        }
+    });
+    for (let i = Math.floor(sum / 2); i >= 0; i--) {
+        if (dp[i]) {
+            return sum - i - i;
+        }
+    }
+    return sum;
+}
+
+console.log(lastStoneWeightII([2,7,4,1,8,1]));
