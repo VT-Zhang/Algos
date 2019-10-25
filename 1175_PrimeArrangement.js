02fbf4fcdf81a1a7e6f7d63e804ba89e925dd262
VT-Zhang <jianz@vt.edu>
2019-10-25 17:24:53 -0400

    1175 Prime Arragement

--- 1175_PrimeArrangement.js
+++ 1175_PrimeArrangement.js
@@ -5,40 +5,25 @@
 const M = 10 ** 9 + 7;
 
 function numPrimeArrangements(n) {
-    const nPrimes = findNumberOfPrimes(n);
-    const memo = {};
-    return (factorial(nPrimes, memo) * factorial(n - nPrimes, memo)) % M;
-}
-
-function factorial(n, memo) {
-    if (n in memo) {
-        return memo[n];
+    let count = 0;
+    const MOD = BigInt(1e9 + 7);
+    const factorials = [1n];
+    for (let i = 1; i < n; i++) {
+        factorials[i] = BigInt(factorials[i - 1] * BigInt(i) % MOD);
     }
-    if (n <= 1) {
-        return 1;
-    }
-    memo[n] = (n * factorial(n - 1, memo)) % M;
-    return memo[n];
-}
-
-function findNumberOfPrimes(n) {
-    let nPrimes = 0;
     for (let i = 2; i <= n; i++) {
-        if (isPrime(i)) {
-            nPrimes += 1;
-        }
+        count += isPrime(i);
     }
-    return nPrimes;
-}
+    return factorials[count] * factorials[n - count] % MOD;
 
-function isPrime(n) {
-    const max = Math.floor(Math.sqrt(n)) + 1;
-    for (let i = 2; i < max; i++) {
-        if (n % i === 0) {
-            return false;
+    function isPrime(m) {
+        for (let i = 2; i * i <= m; i++) {
+            if (m % i === 0) {
+                return false;
+            }
         }
+        return true;
     }
-    return true;
 }
 
 console.log(numPrimeArrangements(5));
