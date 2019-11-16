eef50dbca3c8701c590ffaabfba1d7b4339733f6
VT-Zhang <jianz@vt.edu>
2019-11-16 15:31:00 -0500

    083 Remove Duplicates from Sorted List

---
+++ Java/083_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
